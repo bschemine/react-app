b53f63c82be0de45e3a850245c59523f297c78ba
bschemine <blake.schemine@costrategix.com>
2022-06-08 10:49:30 -0400

    Error

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -2,7 +2,7 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
-  //throw( new Error("some error message"));
+  throw( new Error("some error message"));
 
   const name = core.getInput("who-to-greet");
   console.log(`Hello ${name}`);
@@ -12,5 +12,5 @@ try {
 
   console.log(JSON.stringify(github, null, "\t"));
 } catch (error) {
-  core.setFailed("failed message");
+  core.setFailed(error.message);
 }
